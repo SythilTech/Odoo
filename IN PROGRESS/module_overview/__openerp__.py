116fa68635dae6289bb7d786c030345ff58ffa30
SythilTech <steven@sythiltech.com>
2016-02-14 23:58:57 +1100

    getting tired manually looking modules to find out what they do...
    getting tired manually looking modules to find out what they do...

---
+++ IN PROGRESS/module_overview/__openerp__.py
@@ -0,0 +1,18 @@
+{
+    'name': "Module Overview",
+    'version': "1.0",
+    'author': "Sythil",
+    'category': "Tools",
+    'summary': "Gives an overview of what a module does / has in it",
+    'license':'LGPL-3',
+    'data': [
+        'views/module_overview_templates.xml',
+        'data/website.menu.csv',
+    ],
+    'demo': [],
+    'depends': ['website'],
+    'images':[
+        'static/description/1.jpg',
+    ],
+    'installable': True,
+}
