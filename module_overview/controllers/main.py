755a70db696c1039ccf181f0036a45da7becdd4e
SythilTech <steven@sythiltech.com>
2016-02-18 08:04:54 +1100

    Add Access rules to overview
    Add Access rules to overview
    Want to clean up the interface abit, in particular organize access rules
    better so thier per model and per group like my v7 documnetation

--- module_overview/controllers/main.py
+++ module_overview/controllers/main.py
@@ -4,6 +4,7 @@ import openerp.http as http
 from openerp.http import request
 import requests
 import urllib
+import StringIO
 import os
 from os.path import expanduser
 from datetime import datetime
@@ -12,6 +13,7 @@ import logging
 _logger = logging.getLogger(__name__)
 import zipfile
 import io
+import csv
 
 class ModuleOverView(http.Controller):
 
@@ -46,6 +48,12 @@ class ModuleOverView(http.Controller):
         upload = values['upload']
         module_name = os.path.splitext(upload.filename)[0]
 
+        #Delete the existing module record
+        delete_module = request.env['module.overview'].search([('name','=',module_name)])
+        
+        if delete_module:
+            delete_module[0].unlink()
+        
         module_overview = request.env['module.overview'].create({'name': module_name})
 
         myzipfile = zipfile.ZipFile(io.BytesIO(field_value.read()))
@@ -56,8 +64,10 @@ class ModuleOverView(http.Controller):
             if file_extension == ".py":
                 _logger.error("not supported")
                 #self._read_py(zippy.read(), module_overview.id)
-            elif  file_extension == ".xml":
+            elif file_extension == ".xml":
                 self._read_xml(zippy.read(), module_overview.id)
+            elif file_extension == ".csv":
+                self._read_csv(filename, zippy.read(), module_overview.id)
                 
                 
 	
@@ -66,16 +76,59 @@ class ModuleOverView(http.Controller):
     def _read_py(self, file_content, m_id):        
         return ""
         
+    def _read_csv(self, file_name, file_content, m_id):        
+        if "ir.model.access":
+            rownum = 0
+            reader = csv.reader(StringIO.StringIO(file_content), delimiter=',')
+            header = []
+            for row in reader:
+                row_dict = {'mo_id': m_id}
+                # Save header row.
+                if rownum == 0:
+                    header = row
+                else:
+                    colnum = 0
+                    for col in row:
+                         #map the csv header columns to the fields 
+                         if header[colnum] == "id":
+                             row_dict['x_id'] = col
+                         elif header[colnum] == "model_id:id":
+                             row_dict['model'] = col                         
+                         elif header[colnum] == "group_id:id":
+                             row_dict['group'] = col                          
+                         elif header[colnum] == "perm_read":
+                             row_dict['perm_read'] = bool(int(col))                          
+                         elif header[colnum] == "perm_write":
+                             row_dict['perm_write'] = bool(int(col))                          
+                         elif header[colnum] == "perm_create":
+                             row_dict['perm_create'] = bool(int(col))                          
+                         elif header[colnum] == "perm_unlink":
+                             row_dict['perm_unlink'] = bool(int(col))                       
+                         else:                         
+                             row_dict[header[colnum]] = col
+                         
+                         colnum += 1
+                
+                    #Create a access rule record using the premade dictionary from the csv
+                    request.env['module.overview.access'].create(row_dict)
+                rownum += 1
+       
     def _read_xml(self, file_content, m_id):
         return_string = ""
         root = etree.fromstring(file_content)
         
         insert_menu = root.xpath('//menuitem')
         for menu in insert_menu:
-            x_id = menu.attrib['id']
-            name = menu.attrib['name']
-            parent = menu.attrib['parent']
-            request.env['module.overview.menu'].create({'mo_id': m_id, 'x_id': x_id, 'name': name, 'parent': parent})
+            menu_dict = {'mo_id': m_id}
+            menu_dict['x_id'] = menu.attrib['id']
+
+            if 'name' in menu.attrib:
+                menu_dict['name'] = menu.attrib['name']
+
+            if 'parent' in menu.attrib:
+                menu_dict['parent'] = menu.attrib['parent']
+
+            request.env['module.overview.menu'].create(menu_dict)
         
         insert_records = root.xpath('//record')
         for rec in insert_records:
--- module_overview/models/module_overview.py
+++ module_overview/models/module_overview.py
@@ -8,7 +8,48 @@ class ModuleOverview(models.Model):
     name = fields.Char(string="Module Name")
     models_ids = fields.One2many('module.overview.model', 'mo_id', string="Models")
     menu_ids = fields.One2many('module.overview.menu', 'mo_id', string="Menus")
+    access_ids = fields.One2many('module.overview.access', 'mo_id', string="Access Rules")
+    model_count = fields.Integer(string="Model Count", compute="_compute_model_count")
 
+    @api.one
+    @api.depends('models_ids')
+    def _compute_model_count(self):
+        self.model_count = len(self.models_ids)
+    
+class ModuleOverviewAccess(models.Model):
+
+    _name = "module.overview.access"
+    
+    mo_id = fields.Many2one('module.overview', string="Module Overview")
+    x_id = fields.Char(string="XML ID")
+    name = fields.Char(string="Name")
+    model = fields.Char(string="Model")
+    group = fields.Char(string="Group")
+    perm_read = fields.Boolean(string="Read Permmision")
+    perm_write = fields.Boolean(string="Write Permmision")
+    perm_create = fields.Boolean(string="Create Permmision")
+    perm_unlink = fields.Boolean(string="Delete Permmision")
+    access_string = fields.Char(string="Access String", compute="_compute_access_string")
+    
+    @api.one
+    @api.depends('perm_read', 'perm_write', 'perm_create', 'perm_unlink')
+    def _compute_access_string(self):
+        a_string = ""
+        
+        if self.perm_read:
+            a_string += "Read, "
+
+        if self.perm_write:
+            a_string += "Write, "
+
+        if self.perm_create:
+            a_string += "Create, "
+
+        if self.perm_unlink:
+            a_string += "Delete, "
+            
+        self.access_string = a_string[:-2]
+       
 class ModuleOverviewMenu(models.Model):
 
     _name = "module.overview.menu"
