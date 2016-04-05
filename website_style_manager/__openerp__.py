0df512b970667349b0e0f8662c54554398bf1952
SythilTech <steven@sythiltech.com>
2016-04-06 09:26:34 +1000

    Manage css styles in a central location
    Manage css styles in a central location

---
+++ website_style_manager/__openerp__.py
@@ -0,0 +1,21 @@
+{
+    'name': "Website Style Manager",
+    'version': "1.0",
+    'author': "Sythil",
+    'category': "Tools",
+    'summary': "Apply site wide styles to various HTML tags",
+    'description': "Apply site wide styles to various HTML tags",
+    'license':'LGPL-3',
+    'data': [
+        'data/website.style.fontfamily.csv',
+        'data/website.style.htmltag.csv',
+        'views/website.xml',
+        'views/website_templates.xml',
+    ],
+    'demo': [],
+    'images':[
+        'static/description/1.jpg',
+    ],
+    'depends': ['website'],
+    'installable': True,
+}
---
+++ website_style_manager/models/website.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+from openerp import api, fields, models
+
+class WebsiteStyleManager(models.Model):
+
+    _inherit = "website"
+    
+    tag_styles = fields.One2many('website.style', 'website_id', string="HTML Tag Styles")
+    css_text = fields.Text(string="CSS Text")
+    
+    @api.onchange('tag_styles')
+    def _onchange_tag_styles(self):
+        self.css_text = ""
+        for style in self.tag_styles:
+            self.css_text += style.html_tag.html_tag + " {\n"
+            
+            if style.font_family:
+                self.css_text += "    font-family: '" + style.font_family.name + "';\n"
+    
+            if style.font_color:
+                self.css_text += "    color: " + style.font_color + ";\n"
+
+            if style.font_size:
+                self.css_text += "    font-size: " + style.font_size + ";\n"
+    
+            self.css_text += "}\n\n"
+    
+class WebsiteStyle(models.Model):
+
+    _name = "website.style"
+    
+    website_id = fields.Many2one('website', string="Website")
+    html_tag = fields.Many2one('website.style.htmltag', string="HTML Tag", required=True)
+    font_family = fields.Many2one('website.style.fontfamily', string="Font Family")
+    font_color = fields.Char(string="Font Color", default="#000000")
+    font_size = fields.Char(string="Font Size", help="The Size of the font, please include the suffix", default="16px")
+    
+class WebsiteStyleHTMLTag(models.Model):
+
+    _name = "website.style.htmltag"
+    
+    style_id = fields.Many2one('website.style', string="Website Style")
+    name = fields.Char(string="Name")
+    html_tag = fields.Char(string="HTML Tag", help="Name is for fancy display 'Heading 1' this is the actual tag 'h1'")
+    
+class WebsiteStyleFontFamily(models.Model):
+
+    _name = "website.style.fontfamily"
+    
+    style_id = fields.Many2one('website.style', string="Website Style")
+    name = fields.Char(string="Font Family")
+    
+class WebsiteStyleFontSize(models.Model):
+
+    _name = "website.style.fontsize"
+    
+    style_id = fields.Many2one('website.style', string="Website Style")
+    name = fields.Char(string="Font Size", help="The Size of the font, please have em, px, % or pt suffix", default="12px")
