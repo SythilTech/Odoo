1e59b5c1519c05bbb4685eb6fda06f705df5d4da
SythilTech <steven@sythiltech.com>
2016-05-17 19:26:34 +1000

    Opps didn't realise it didn't install..
    Opps didn't realise it didn't install..

--- mail_campaign_survey/models/marketing_survey_template.py
+++ mail_campaign_survey/models/marketing_survey_template.py
@@ -6,13 +6,17 @@ from openerp import api, fields, models
 class MarketingSurveyTemplate(models.Model):
 
     _name = "marketing.survey.template"
-    _inherit = "mail.compose.message"
+    #_inherit = "mail.compose.message"
     _rec_name = "name"
+
+    def _default_email_from(self):
+        return self.env.user.email
     
     name = fields.Char(string="Name", related="survey_id.title")
     survey_id = fields.Many2one('survey.survey', string="Survey", required="True")
     subject = fields.Char(string="Subject", required="True")
     body = fields.Html(string="Contents", required="True")
+    email_from = fields.Char(string="From Email", default=_default_email_from)
     
     @api.onchange('survey_id')
     def _onchange_survey_id(self):
