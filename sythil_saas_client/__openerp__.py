8a7daab0b089c0600826c99fc5d8c943e0806462
SythilTech <steven@sythiltech.com>
2016-08-14 17:00:20 +1000

    I give up, was meant to enable app store for admin only but javascript hates me
    I give up, was meant to enable app store for admin only but javascript
    hates me

--- sythil_saas_client/__openerp__.py
+++ sythil_saas_client/__openerp__.py
@@ -1,19 +1,19 @@
 {
-    'name': "Sythil Tech Client",
-    'version': "1.0",
+    'name': "Sythil Tech SAAS Client",
+    'version': "1.0.1",
     'author': "Sythil",
     'category': "Tools",
     'summary':'Interacts with Sythil SAAS Server',
     'license':'LGPL-3',
+    'depends': ['web'],
     'data': [
         'data/res.users.csv',
         'data/ir.rule.xml',
-        'data/ir.config_parameter.csv',
+        'views/sythil_saas_client_templates.xml',
     ],
     'demo': [],
     'images':[
         'static/description/1.jpg',
     ],
-    'depends': [],
     'installable': True,
 }
---
+++ sythil_saas_client/static/src/js/sythil_saas_client.js
@@ -0,0 +1,49 @@
+console.log("1");
+odoo.define('sythil_saas_client.sythil_saas_client', function (require) {
+"use strict";
+
+console.log("2");
+
+    var apps = require('base.apps');
+
+    var admin_only_app_store = apps.Apps.include({
+    get_client: function() {
+		console.log("test");
+        // return the client via a deferred, resolved or rejected depending if the remote host is available or not.
+        var check_client_available = function(client) {
+            var d = $.Deferred();
+            var i = new Image();
+            i.onerror = function() {
+                d.reject(client);
+            };
+            i.onload = function() {
+                d.resolve(client);
+            };
+            var ts = new Date().getTime();
+            i.src = _.str.sprintf('%s/web/static/src/img/sep-a.gif?%s', client.origin, ts);
+            return d.promise();
+        };
+        if (apps_client) {
+            return check_client_available(apps_client);
+        } else {
+            var Mod = new Model('ir.module.module');
+            return Mod.call('get_apps_server').then(function(u) {
+                var link = $(_.str.sprintf('<a href="%s"></a>', u))[0];
+                var host = _.str.sprintf('%s//%s', link.protocol, link.host);
+                var dbname = link.pathname;
+                if (dbname[0] === '/') {
+                    dbname = dbname.substr(1);
+                }
+                var client = {
+                    origin: host,
+                    dbname: dbname
+                };
+                apps_client = client;
+                return check_client_available(client);
+            });
+        }
+    },
+
+    });
+
+});
