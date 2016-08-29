3420d4ec8647f0a7c43d911259b6443f989452d1
SythilTech <steven@sythiltech.com>
2016-08-29 17:48:03 +1000

    stores multi line log entires
    stores multi line log entires

--- error_tracker/__openerp__.py
+++ error_tracker/__openerp__.py
@@ -1,6 +1,6 @@
 {
     'name': "Error Tracker",
-    'version': "1.0",
+    'version': "1.1",
     'author': "Sythil",
     'category': "Tools",
     'summary':'Assists in the resolution of extermly ambiguous error messages',
--- error_tracker/models/error_tracker.py
+++ error_tracker/models/error_tracker.py
@@ -7,9 +7,10 @@ from openerp import api, fields, models
 class ErrorTracker(models.TransientModel):
 
     _name = "error.tracker"
+    _order = 'line_number desc'
     
     error_date = fields.Datetime(string='Date')
-    level = fields.Selection([('INFO', 'Info'), ('WARNING', 'Warning'), ('ERROR', 'Error')], string="Level")
+    level = fields.Selection([('INFO', 'Info'), ('WARNING', 'Warning'), ('ERROR', 'Error'), ('CRITICAL', 'Critical')], string="Level")
     database = fields.Char(string="Database")
     error_class = fields.Char(string="Class")
     class_error = fields.Char(string="Class Error")
@@ -25,12 +26,21 @@ class ErrorTracker(models.TransientModel):
             lines = f.readlines()
             
         line_number = 0
+        error_content = ""
         for line in lines:
             line_number += 1
-            if "INFO" in line or "WARNING" in line or "ERROR" in line:
+            if " INFO " in line or " WARNING " in line or " ERROR " in line or " CRITICAL " in line:
+                if error_content <> "":
+                   log_entry.error_content = error_content
+                   error_content = ""
+                
                 error_date = line.split(' ')[0] + " " + line.split(' ')[1].split(",")[0]
                 level = line.split(' ')[3]
                 database = line.split(' ')[4]
                 error_class = line.split(' ')[5][:-1]
                 class_error = line.split(":")[3]
-                self.env['error.tracker'].create({'line_number': line_number, 'error_date':  error_date, 'level': level, 'database': database, 'error_class': error_class, 'class_error': class_error})
+                
+                log_entry = self.env['error.tracker'].create({'line_number': line_number, 'error_date':  error_date, 'level': level, 'database': database, 'error_class': error_class, 'class_error': class_error})
+            else:
+                error_content += line
+            
