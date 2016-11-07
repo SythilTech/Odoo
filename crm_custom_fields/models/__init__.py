865955c44208ba3d76672ad1f7d755d03025627c
SythilTech <steven@sythiltech.com>
2016-11-07 11:51:23 +1100

    Version 10 of the business directory
    Version 10 of the business directory
    
    Only a small percentage of my modules will be upgraded to version 10
    since new modules have made alot of them obsolete or I wish to revise
    other ways to find solutions for for existing desires, i.e voip rather
    then twilio

--- crm_custom_fields/models/__init__.py
+++
@@ -1,1 +0,0 @@
-import ir_model, ir_model_fields, res_partner
---
+++ website_business_directory/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Website Business Directory",
+    'version': "0.6.1",
+    'author': "Sythil Tech",
+    'category': "Tools",
+    'summary': "A directory of local companies",
+    'license':'LGPL-3',
+    'data': [
+        'views/res_partner_views.xml',
+        'views/website_business_directory_templates.xml',
+        'views/res_partner_directory_department_views.xml',
+        'data/website.menu.csv',
+        'data/res.groups.csv',
+        'security/ir.model.access.csv',
+    ],
+    'demo': [],
+    'depends': ['mail','website'],
+    'images':[
+        'static/description/1.jpg',
+    ],
+    'installable': True,
+}
