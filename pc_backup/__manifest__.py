4d886ae5bf582e7387c4278cb543f051e575e38d
SythilTech <steven@sythiltech.com>
2016-11-26 18:49:01 +1100

    version 10 of module
    version 10 of module
    Will continue to develop this until it becomes a viable backup solution
    for micro startup businesses

---
+++ pc_backup/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': "PC Backup",
+    'version': "0.1",
+    'author': "Sythil Tech",
+    'category': "Tools",
+    'summary': "Backs up important files on your desktop computers",
+    'description': "Backs up important files on your desktop computers",
+    'license':'LGPL-3',
+    'data': [
+        'views/backup_computer_views.xml',
+        'views/backup_computer_file_views.xml',
+        'views/pc_backup_templates.xml',
+    ],
+    'demo': [],
+    'depends': ['web'],
+    'images':[
+        'static/description/1.jpg',
+    ],
+    'installable': True,
+}
