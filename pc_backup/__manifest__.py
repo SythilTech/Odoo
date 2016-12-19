1e4faf111139c41d0065bb55f48960c8f26a8efd
SythilTech <steven@sythiltech.com>
2016-12-19 14:57:41 +1100

    Remove backup read permissions
    Remove backup read permissions

--- pc_backup/__manifest__.py
+++ pc_backup/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': "PC Backup",
-    'version': "0.2",
+    'version': "0.2.1",
     'author': "Sythil Tech",
     'category': "Tools",
     'summary': "Backs up important files on your desktop computers",
