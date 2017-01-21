129844c9099c2a8b5974440bc6d24d5f91be85a9
SythilTech <steven@sythiltech.com>
2017-01-21 14:51:57 +1100

    Change ticket seq number in company form view
    Change ticket seq number in company form view

--- website_support/__openerp__.py
+++ website_support/__openerp__.py
@@ -18,6 +18,7 @@
         'views/website_support_help_groups_views.xml',
         'views/website_support_help_page_views.xml',
         'views/website_support_ticket_priority_views.xml',
+        'views/res_company_views.xml',
         'data/website.support.ticket.states.csv',
         'data/website.support.ticket.categories.csv',
         'data/website.menu.csv',
