fa4f4ab209c972790b9315375858386ea89a65ba
SythilTech <steven@sythiltech.com>
2017-04-02 10:33:43 +1000

    standardise formating so searching by mobile is easier
    standardise formating so searching by mobile is easier

--- sms_frame/models/res_partner.py
+++ sms_frame/models/res_partner.py
@@ -11,10 +11,10 @@ class ResPartnerSms(models.Model):
         """Tries to convert a local number to e.164 format based on the partners country, don't change if already in e164 format"""
         if self.mobile and self.country_id and self.country_id.mobile_prefix:
             if self.mobile.startswith("0"):
-                self.mobile = self.country_id.mobile_prefix + self.mobile[1:]
+                self.mobile = self.country_id.mobile_prefix + self.mobile[1:].replace(" ","")
             elif self.mobile.startswith("+"):
-                self.mobile = self.mobile
+                self.mobile = self.mobile.replace(" ","")
             else:
-                self.mobile = self.country_id.mobile_prefix + self.mobile
+                self.mobile = self.country_id.mobile_prefix + self.mobile.replace(" ","")
         else:
-            self.mobile = self.mobile
+            self.mobile = self.mobile.replace(" ","")
