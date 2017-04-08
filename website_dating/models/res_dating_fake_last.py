543913c85877b2cb43bf3f02a36165cf04832692
Steven Wright <steven@sythiltech.com.au>
2017-04-08 23:35:01 +1000

    While I do still want to help people who feel lonely, I plan to change my appoach to a non dating based one
    While I do still want to help people who feel lonely, I plan to change
    my appoach to a non dating based one

--- website_dating/models/res_dating_fake_last.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from openerp import api, fields, models
-
-class ResDatingFakeLast(models.Model):
-
-    _name = "res.dating.fake.last"
-    
-    name = fields.Char(string='First Name')
-    amount = fields.Integer(string="Amount")
