7b16bd3b2675624fbe35c3b7b1720b499ccdcf8e
Steven Wright <steven@sythiltech.com.au>
2017-05-03 18:21:53 +1000

    fix empty mobile replace issue
    fix empty mobile replace issue

--- sms_frame/models/res_partner.py
+++ sms_frame/models/res_partner.py
@@ -8,12 +8,14 @@ class ResPartnerSms(models.Model):
     @api.onchange('country_id','mobile')
     def _onchange_mobile(self):
         """Tries to convert a local number to e.164 format based on the partners country, don't change if already in e164 format"""
-        if self.mobile and self.country_id and self.country_id.mobile_prefix:
-            if self.mobile.startswith("0"):
-                self.mobile = self.country_id.mobile_prefix + self.mobile[1:].replace(" ","")
-            elif self.mobile.startswith("+"):
-                self.mobile = self.mobile.replace(" ","")
+        if self.mobile:                    
+            
+            if self.country_id and self.country_id.mobile_prefix:
+                if self.mobile.startswith("0"):
+                    self.mobile = self.country_id.mobile_prefix + self.mobile[1:].replace(" ","")
+                elif self.mobile.startswith("+"):
+                    self.mobile = self.mobile.replace(" ","")
+                else:
+                    self.mobile = self.country_id.mobile_prefix + self.mobile.replace(" ","")
             else:
-                self.mobile = self.country_id.mobile_prefix + self.mobile.replace(" ","")
-        else:
-            self.mobile = self.mobile.replace(" ","")
+                self.mobile = self.mobile.replace(" ","")
