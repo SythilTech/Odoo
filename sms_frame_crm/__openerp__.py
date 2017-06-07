91b54445f44df45c50133eeab8de0973eb1f83c0
Steven Wright <steven@sythiltech.com.au>
2017-06-07 20:39:52 +1000

    port back to v9 from v10
    port back to v9 from v10

---
+++ sms_frame_crm/__openerp__.py
@@ -0,0 +1,19 @@
+{
+    'name': "SMS Framework - CRM",
+    'version': "1.0",
+    'author': "Sythil Tech",
+    'category': "Tools",
+    'support': "steven@sythiltech.com.au",
+    'summary':'Allows sending smses from / to leads',
+    'description':'Allows sending smses from / to leads',    
+    'license':'LGPL-3',
+    'data': [
+        'views/crm_lead_views.xml',
+    ],
+    'demo': [],
+    'depends': ['sms_frame', 'crm'],
+    'images':[
+        'static/description/1.jpg',
+    ],
+    'installable': True,
+}
