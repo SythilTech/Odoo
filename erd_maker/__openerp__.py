90ef8735c88cbb89d6336d6dd11768805215ab45
Steven Wright <steven@sythiltech.com.au>
2017-06-11 09:31:36 +1000

    port to v10
    port to v10

---
+++ erd_maker/__openerp__.py
@@ -0,0 +1,20 @@
+{
+    'name': "Entity Relationship Diagram(ERD) Maker",
+    'version': "1.0",
+    'author': "Sythil Tech",
+    'category': "Tools",
+    'support': "steven@sythiltech.com.au",
+    'summary':'Creates an Entity Relationship Diagram (ERD)',
+    'license':'LGPL-3',
+    'data': [
+        'security/ir.model.access.csv',
+        'views/erd_maker.xml',
+        'views/erd_maker_module.xml',
+    ],
+    'demo': [],
+    'depends': [],
+    'images':[
+        'static/description/1.jpg',
+    ],
+    'installable': True,
+}
