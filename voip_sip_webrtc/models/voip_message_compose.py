3d6a08f9c708e1bd1ac858ea29d8d2a87afc5cfe
Steven Wright <steven@sythiltech.com.au>
2017-10-01 20:57:34 +1100

    xmpp message start
    xmpp message start
    
    I'm getting used to working on the protocol level, I just wish I had
    more time / energy to work on this project.

--- voip_sip_webrtc/models/voip_message_compose.py
+++ voip_sip_webrtc/models/voip_message_compose.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import socket
+import threading
 import logging
 _logger = logging.getLogger(__name__)
 
@@ -11,6 +13,39 @@ class VoipMessageCompose(models.TransientModel):
     type = fields.Char(string="Message Type")
     partner_id = fields.Many2one('res.partner', string="Partner")
     message = fields.Text(string="Message")
-    
+
+    def xmpp_listener(self, to_address):
+                
+        _logger.error("Start XMMP Listening")
+
+        try:
+            account = to_address.split("@")[0]
+            domain = to_address.split("@")[1]
+
+            xmpp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+            xmpp_socket.connect( ('216.189.159.196', 5222) )
+            #xmpp_socket.connect((domain, 5222))
+            
+            send_data = ""
+            send_data += '<?xml version="1.0"?>'
+            send_data += '<stream:stream xmlns="jabber:client" xmlns:stream="http://etherx.jabber.org/streams" to="jabberzac.org" version="1.0">'
+            #send_data += '<stream:stream xmlns="jabber:client" xmlns:stream="http://etherx.jabber.org/streams" to="' + str(domain) + '" version="1.0">'
+
+            _logger.error("Before Send")        
+            xmpp_socket.send(send_data)
+            _logger.error("After Send")
+
+            _logger.error( xmpp_socket.recv(1024) )
+            xmpp_socket.close             
+
+        except Exception as e:
+            _logger.error(e)
+
+        
     def send_message(self):
         _logger.error("Send Message")
+
+        to_address = self.partner_id.xmpp_address
+        xmpp_listener_starter = threading.Thread(target=self.xmpp_listener, args=(to_address,))
+        xmpp_listener_starter.start()        
