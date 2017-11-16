4ffee03721b913d0d3e27fbb08351e633db28a80
Steven Wright <steven@sythiltech.com.au>
2017-11-16 20:10:14 +1100

    * Add customer name field
    * Add customer name field

--- website_support_analytic_timesheets/doc/changelog.rst
+++ website_support_analytic_timesheets/doc/changelog.rst
@@ -1,3 +1,7 @@
+v1.0.7
+======
+* Add customer name field
+
 v1.0.6
 ======
 * Disable create button for analytic line
--- website_support_analytic_timesheets/models/account_analytic_line.py
+++ website_support_analytic_timesheets/models/account_analytic_line.py
@@ -6,6 +6,7 @@ class WebsiteSupportTicketInheritAccountAnalyticLine(models.Model):
     _inherit = "account.analytic.line"
 
     support_ticket_id = fields.Many2one('website.support.ticket', string="Support Ticket")
+    person_name = fields.Char(related="support_ticket_id.person_name", string="Customer Name")
     ticket_number_display = fields.Char(related="support_ticket_id.ticket_number_display", string="Ticket Number")
     state = fields.Many2one('website.support.ticket.states', readonly=True, related="support_ticket_id.state", string="State")
     open_time = fields.Datetime(related="support_ticket_id.create_date", string="Open Time")    
