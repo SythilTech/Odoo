53ad92ef5451ffbbd7652e611b280b787499d52b
Steven Wright <steven@sythiltech.com.au>
2017-12-06 18:52:57 +1100

    transcode wizard on SIP account screen
    transcode wizard on SIP account screen

--- voip_sip_webrtc_transcode/__manifest__.py
+++ voip_sip_webrtc_transcode/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': "Voip Communication - Transcoding",
-    'version': "1.0.0",
+    'version': "1.0.1",
     'author': "Sythil Tech",
     'category': "Tools",
     'support': "steven@sythiltech.com.au",
@@ -10,6 +10,8 @@
     'data': [
         'views/voip_call_template_transcode_wizard_views.xml',
         'views/voip_call_template_views.xml',
+        'views/voip_account_transcode_wizard_views.xml',
+        'views/voip_account_views.xml',
     ],
     'demo': [],
     'depends': ['voip_sip_webrtc'],
