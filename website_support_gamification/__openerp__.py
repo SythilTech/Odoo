26d64415d54a62fadd4c5444dc1ccc2d40aef62e
Steven Wright <steven@sythiltech.com.au>
2018-03-26 08:16:21 +1100

    quick port over to version 11
    quick port over to version 11

---
+++ website_support_gamification/__openerp__.py
@@ -0,0 +1,18 @@
+{
+    'name': "Website Help Desk / Support Ticket - Gamification",
+    'version': "1.0",
+    'author': "Sythil Tech",
+    'category': "Tools",
+    'support': "steven@sythiltech.com.au",
+    'summary': "Adds badges to support tickets",
+    'description': "Adds badges to support tickets",
+    'license':'LGPL-3',
+    'data': [
+        'data/gamification_data.xml',
+    ],
+    'demo': [],
+    'depends': ['website_support_analytic_timesheets', 'gamification'],
+    'images':[
+    ],
+    'installable': True,
+}
