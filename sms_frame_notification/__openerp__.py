8cf88ff269726cb340d7c96ad1b1ea1a4cd8b962
Steven Wright <steven@sythiltech.com.au>
2019-03-13 11:59:21 +1100

    Allow non technical users access to sms messages
    Allow non technical users access to sms messages

---
+++ sms_frame_notification/__openerp__.py
@@ -0,0 +1,19 @@
+{
+    'name': "SMS Framework - Notification",
+    'version': "1.0.0",
+    'author': "Sythil Tech",
+    'category': "Tools",
+    'support': "steven@sythiltech.com.au",
+    'summary':'Allow non technical users access to sms messages',
+    'description':'Allow non technical users access to sms messages',
+    'license':'LGPL-3',
+    'data': [
+        'security/ir.model.access.csv',
+        'views/sms_message_views.xml',
+    ],
+    'depends': ['sms_frame'],
+    'images':[
+        'static/description/1.jpg',
+    ],
+    'installable': True,
+}
